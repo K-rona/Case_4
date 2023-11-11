c2da2f59947d255e9855ba4c9a2b8e7ada87ee82
K-rona <126541689+K-rona@users.noreply.github.com>
2023-11-11 20:15:54 +0700

    Update Case.py

--- Case.py
+++ Case.py
@@ -8,44 +8,6 @@ symbols = ".?!"
 count = 0
 dictionary = {}
 
-for i in range(len(words)):
-
-    if words[i] in dictionary:
-        dictionary[words[i]].append(words[i+1])
-
-    else:
-        dictionary[words[i]] = []
-
-first_word = np.random.choice(words)
-for i in symbols:
-    if i in first_word:
-        count += 1
-
-result = [first_word]
-
-while count <= number:
-
-    next_word = np.random.choice(dictionary[first_word[-1]])
-    result.append(next_word)
-
-    for i in symbols:
-        if i in next_word:
-            count += 1
-
-print(' '.join(result))
-
-
-
-with open('input.txt', 'r', encoding='utf-8') as f_in:
-    number = int(f_in.readline().rstrip())
-    text = f_in.read()
-
-words = text.split()
-
-symbols = ".?!"
-count = 0
-dictionary = {}
-
 for word1 in range(len(words) - 1):
     word2 = word1 + 1
 
@@ -57,6 +19,7 @@ for word1 in range(len(words) - 1):
 print(dictionary)
 
 first_word = np.random.choice(words)
+
 for i in symbols:
     if i in first_word:
         count += 1
@@ -65,7 +28,7 @@ result = [first_word]
 
 while count <= number:
 
-    next_word = np.random.choice(dictionary[first_word[-1]])
+    next_word = np.random.choice(dictionary[result[-1]])
     result.append(next_word)
 
     for i in symbols:
