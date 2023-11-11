042ce3fabc1a1e853779854363f66facae3a1447
K-rona <126541689+K-rona@users.noreply.github.com>
2023-11-11 20:07:12 +0700

    Update Case.py

--- Case.py
+++ Case.py
@@ -33,3 +33,43 @@ while count <= number:
             count += 1
 
 print(' '.join(result))
+
+
+
+with open('input.txt', 'r', encoding='utf-8') as f_in:
+    number = int(f_in.readline().rstrip())
+    text = f_in.read()
+
+words = text.split()
+
+symbols = ".?!"
+count = 0
+dictionary = {}
+
+for word1 in range(len(words) - 1):
+    word2 = word1 + 1
+
+    if words[word1] in dictionary.keys():
+        dictionary[words[word1]].append(words[word2])
+    else:
+        dictionary[words[word1]] = [words[word2]]
+
+print(dictionary)
+
+first_word = np.random.choice(words)
+for i in symbols:
+    if i in first_word:
+        count += 1
+
+result = [first_word]
+
+while count <= number:
+
+    next_word = np.random.choice(dictionary[first_word[-1]])
+    result.append(next_word)
+
+    for i in symbols:
+        if i in next_word:
+            count += 1
+
+print(' '.join(result))
